f4bbc3652d0eb8ad084261eb09b108fcd3f0b0c1
thiagomatte <135778709+thiagomatte@users.noreply.github.com>
2024-07-04 22:38:22 -0300

    Update UserRepository.java

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByUsername(String username);
 }
